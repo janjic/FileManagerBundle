27b44071a1c861e5da5cb6eba8233eda84414969
Đorđe Knežević <djordje@fsd.rs>
2018-09-13 12:57:15 +0200

    Update FileManagerEvents.php

--- Event/FileManagerEvents.php
+++ Event/FileManagerEvents.php
@@ -17,6 +17,8 @@ final class FileManagerEvents
     /** @Event("Symfony\Component\EventDispatcher\GenericEvent") */
     const POST_DELETE_FILE = 'file_manager.post_delete_file';
     /** @Event("Symfony\Component\EventDispatcher\GenericEvent") */
+    const POST_DELETE_FILE_DONE = 'file_manager.post_delete_file.done';
+    /** @Event("Symfony\Component\EventDispatcher\GenericEvent") */
     const PRE_DELETE_FOLDER = ' file_manager.pre_delete_folder';
     /** @Event("Symfony\Component\EventDispatcher\GenericEvent") */
     const POST_DELETE_FOLDER = 'file_manager.post_delete_folder';
